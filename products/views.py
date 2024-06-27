a2742e653f2e07cf976d6a983c865ef879c11c9a
Chloe Livingstone-Evans <chloe_c123@hotmail.co.uk>
2024-06-27 15:13:18 +0000

    Added sorting functionality

--- products/views.py
+++ products/views.py
@@ -11,10 +11,26 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     # on form submission, get input field text or give error message and redirect to productspage
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -28,11 +44,14 @@ def all_products(request):
             
             queries = Q(name__icontains=query) | Q(description__icontains=query)  # setting variable to a Q object. uses djangos Q to return a list with name or description matching a query. i means not case sensitive
             products = products.filter(queries) # feed queries variable into filter function
+    
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
