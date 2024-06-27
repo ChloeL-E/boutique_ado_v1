c205c29c0b9bb8cd623e276a92134936a282fe9c
Chloe Livingstone-Evans <chloe_c123@hotmail.co.uk>
2024-06-27 11:55:43 +0000

    Added search functionality

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
@@ -10,8 +10,16 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
+
+    # on form submission, get input field text or give error message and redirect to productspage
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -19,11 +27,12 @@ def all_products(request):
                 return redirect(reverse('products'))
             
             queries = Q(name__icontains=query) | Q(description__icontains=query)  # setting variable to a Q object. uses djangos Q to return a list with name or description matching a query. i means not case sensitive
-            products = products.filter(queries)
+            products = products.filter(queries) # feed queries variable into filter function
 
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
