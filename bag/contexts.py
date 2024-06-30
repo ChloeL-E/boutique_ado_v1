7bbae49f0860a8b4c07b4fd579c3a634eb3529c7
Chloe Livingstone-Evans <chloe_c123@hotmail.co.uk>
2024-06-30 17:22:40 +0000

    Finished product size logic

--- bag/contexts.py
+++ bag/contexts.py
@@ -14,15 +14,27 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int): # Check if item_data is an Integer: This checks if the value associated with item_id is an integer. If it is, it indicates the item does not have different sizes.
+            product = get_object_or_404(Product, pk=item_id) # Retrieve Product: get_object_or_404(Product, pk=item_id) fetches the product from the database using the item_id. If the product doesn't exist, it raises a 404 error.
+            total += item_data * product.price # Calculate Total Price: The total price is updated by adding the quantity of the item times its price.
+            product_count += item_data # Update Product Count: The total count of products is incremented by the quantity of this item.
+            bag_items.append({ # Append to Bag Items: A dictionary containing the item_id, quantity, and product is appended to the bag_items list.
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else: # handles items that have sizes (i.e., item_data is a dictionary).
+            product = get_object_or_404(Product, pk=item_id) # Retrieve Product: it fetches the product object using the item_id or if none exists an error message.
+            for size, quantity in item_data['items_by_size'].items(): # Loop through Sizes: The inner loop iterates over each size and its corresponding quantity in the items_by_size dictionary.
+                total += quantity * product.price # Calculate Total Price: For each size, it updates the total by adding the quantity x the product's price.
+                product_count += quantity # Update Product Count: It increments the product_count by the quantity for each size
+                bag_items.append({ # Append to Bag Items: It appends a dictionary containing the item_id, quantity, product, and size to the bag_items list.
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'product': product,
+                    'size': size,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
--- bag/views.py
+++ bag/views.py
@@ -12,19 +12,35 @@ def view_bag(request):
 def add_to_bag(request, item_id): # form to add_to_bag in product_details.html submits to this view to add product to shopping bag. Takes item_id and quantity(below)
     '''
     Add a quantity of the specified product to the shopping bag.
-    Once in the view will get the bag variable if it exists in session or create if it doesnt.
-    Add the item to the bag or update the quanitty if item already exists. Update variable bag in session. Redirect to redirect_url.
+    The function retrieves the item quantity, redirect URL, and optionally the size from the POST request.
+    It checks the session for an existing shopping bag or creates a new one if none exists.
+    It updates the shopping bag with the specified item and quantity, handling items with and without sizes.
+    The updated shopping bag is saved back into the session.
+    The user is redirected to the specified URL.
     '''
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    # to store the contents of the shopping bag in the http so the contents persist whilst the user shops within site withou losing ocontent of bag
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    # to store the contents of the shopping bag in the http so the contents persist whilst the user shops within site without losing content of bag
     bag = request.session.get('bag', {}) #  variable to access the requests session, trying to get the variable 'bag' if it already exists or initializing to an empty dictionary if none exists
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
-    else:
-        bag[item_id] = quantity
+    if size:
+        if item_id in list(bag.keys()):  #  Item Exists: If the item with the given item_id already exists in the bag:
+            if size in bag[item_id]['items_by_size'].keys():  # Size Exists: If the specific size of the item exists, increment the quantity.
+                bag[item_id]['items_by_size'][size] += quantity
+            else:  # Size Doesn't Exist: Add the size and set the quantity.
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}  # Item Doesn't Exist: Create a new entry for the item with the specified size and quantity.
+    else:  # without size
+        if item_id in list(bag.keys()):  # Item Exists: If the item exists in the bag, simply increment the quantity.
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity  # Item Doesn't Exist: Add the item with the specified quantity.
     
     request.session['bag'] = bag
     return redirect(redirect_url)
+
