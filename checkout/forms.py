cf28c70064746966df1c28df37e83d7ec1121fda
Chloe Livingstone-Evans <chloe_c123@hotmail.co.uk>
2024-07-02 11:07:43 +0000

    Added forms, admin and signals

--- checkout/forms.py
+++ checkout/forms.py
@@ -15,8 +15,8 @@ class OrderForm(forms.ModelForm):
         Add placeholders and classes, remove auto-generated
         labels and set autofocus on first field
         """
-        super().__init__(*args, **kwargs)
-        placeholders = {
+        super().__init__(*args, **kwargs) # call the default __init__ method to set the form up as it would be by default
+        placeholders = { # created a dictionary of placeholders that will appear in form fields rather than clunky looking placeholder and empty text boxes in the template
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
@@ -28,12 +28,12 @@ class OrderForm(forms.ModelForm):
             'county': 'County',
         }
 
-        self.fields['full_name'].widget.attrs['autofocus'] = True
-        for field in self.fields:
+        self.fields['full_name'].widget.attrs['autofocus'] = True  # cursor will start in the full name field when user comes to page
+        for field in self.fields: # iterate through form fields
             if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
+                placeholder = f'{placeholders[field]} *' # add an asterix to all form fields set to required on the model
             else:
                 placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
-            self.fields[field].label = False
+            self.fields[field].widget.attrs['placeholder'] = placeholder # setting all the placeholder attributed to the value in the dictionary above
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input' # adding a css class
+            self.fields[field].label = False # removing the form fields labels as the placeholders are now set
