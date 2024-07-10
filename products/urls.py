53b82380f2e2505c9696e55a8f0745bc76d24cd6
Chloe Livingstone-Evans <chloe_c123@hotmail.co.uk>
2024-07-10 10:50:09 +0000

    Product admin - add product view, url and template

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<product_id>', views.product_detail, name='product_detail')
+    path('<int:product_id>/', views.product_detail, name='product_detail'),  # product id should be an integer. Django will always use the first url it finds to match the pattern for
+    path('add/', views.add_product, name='add_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
+
 from .models import Product, Category
+from .forms import ProductForm
 
 # Create your views here.
 
@@ -66,3 +68,14 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm() # render an empty instance of the form
+    template = 'products/add_product.html' # use the add product template
+    context = { # includes a context including the product form
+        'form': form,
+    }
+
+    return render(request, template, context)  
